c84146885cd20378954c857ed7fc00aae51900f0
Kamrul Hasan <ozdreamer.it@gmail.com>
2020-08-11 23:31:50 +1000

    Changing the vesion to Major Minor

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("Production")]
 [assembly: AssemblyCompany("OzDreamer IT Solutions")]
 [assembly: AssemblyProduct("OZD Smart Exam")]
-[assembly: AssemblyCopyright("Copyright © 2013-20")]
+[assembly: AssemblyCopyright("Copyright © 2013-2020")]
 [assembly: AssemblyTrademark("OzDreamer IT Solutions")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
